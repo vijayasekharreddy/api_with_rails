27a11cc2b6d5da38b58c03291957c64e9884e41d
m.vijaysekhar <m.vijaysekhar@gmail.com>
2015-09-14 12:29:40 +0530

    Adds shoulda matchers for spec refactors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,3 +11,12 @@ describe User do
 
   it { should be_valid }
 end
+
+describe "when email is not present" do
+  before { @user.email = " " }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
+  it { should_not be_valid }
+end
